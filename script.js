68f20d4ffdf4ed58c336d6d02807dce65b729df4
magnusFT <58776797+magnusFT@users.noreply.github.com>
2023-10-03 15:13:59 +0200

    Game over menu added if cactus is hit with dino

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const dinosaur = document.getElementById("dino");
+const kaktus = document.getElementById("kaktus");
 
 function jump(){
-    dinosaur.style.top ="50px";
+    dinosaur.style.top = "50px";
+
+                                              
     setTimeout(function(){
         dinosaur.style.top = "150px";
     }, 275);
 }
 
-document.addEventListener("keydown", jump);
+let isAlive = setInterval(function(){ 
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    let kaktusLeft = parseInt(window.getComputedStyle(kaktus).getPropertyValue("left"));   
+
+if(kaktusLeft < 50 && kaktusLeft > 0 && dinoTop > 140){
+    alert("Game Over!");
+}
+}, 10);
+document.addEventListener("keydown", jump);             
